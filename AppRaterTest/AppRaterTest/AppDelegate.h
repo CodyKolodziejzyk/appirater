2e27247096ea102ab51ff506872a0bc09bd46278
Cody Kolodziejzyk <cody.ko@gmail.com>
2012-10-08 12:24:05 -0700

    Reorganized

---
+++ AppRaterTest/AppRaterTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppRaterTest
+//
+//  Created by Cody Kolodziejzyk on 10/8/12.
+//  Copyright (c) 2012 Cody Kolodziejzyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
